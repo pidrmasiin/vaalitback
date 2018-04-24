de545d7845b0277be91649e55c297b15f8ea0d75
pidrmasiin <petteri.paakkonen@helsinki.fi>
2018-04-24 15:22:27 +0300

    put/delete kysymys & kategoria

--- controllers/kysymys.js
+++ controllers/kysymys.js
@@ -16,6 +16,16 @@ kysymysRouter.get('/', async (request, response) => {
   response.json(kysymykset.map(Kysymys.format))
 })
 
+kysymysRouter.get('/:id', async (request, response) => {
+  try{
+    const kysymys = await Kysymys.findById(request.params.id)
+    response.json(Kysymys.format(kysymys))
+  }catch (exception) {
+    console.log(exception)
+    response.status(400).json({ error: 'kysymystä ei löydy' })
+  }
+})
+
 kysymysRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -49,6 +59,40 @@ kysymysRouter.post('/', async (request, response) => {
 })
 
 
+kysymysRouter.put('/:id', async (request, response) => {
+  try{
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const kysymys = request.body
+    const edited = await Kysymys.findByIdAndUpdate(request.params.id, kysymys, { new: true })
+
+    if(kysymys.kategoriat){
+      for (let i = 0; i < kysymys.kategoriat.length; i += 1) {
+        const kategoria = await Kategoria.findById(kysymys.kategoriat[i])
+        const id = edited._id
+        kategoria.kysymykset = kategoria.kysymykset.concat(id)
+        await kategoria.save()
+      }
+    }
+
+    response.json(edited).end()
+
+  } catch (exception) {
+    if (exception.name === 'JsonWebTokenError' ) {
+      response.status(401).json({ error: exception.message })
+    } else {
+      console.log(exception)
+      response.status(500).json({ error: 'something went wrong...' })
+    }
+  }
+})
+
+
+
 kysymysRouter.get('/:id', async (request, response) => {
 
   try{
@@ -73,6 +117,16 @@ kysymysRouter.delete('/:id', async (request, response) => {
     if (!decodedToken.id || !token) {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
+    const kysymys = await Kysymys.findById(request.params.id)
+    if(kysymys.kategoriat){
+      for (let i = 0; i < kysymys.kategoriat.length; i += 1) {
+        const kategoria = await Kategoria.findById(kysymys.kategoriat[i])
+        const id = kysymys._id
+        kategoria.kysymykset = kategoria.kysymykset.filter(x => x.toString() !== id.toString())
+        await Kategoria.findByIdAndUpdate(kategoria.id, kategoria, { new: true })
+      }
+    }
+
     await Kysymys.findByIdAndRemove(request.params.id)
     response.status(204).end()
   }catch (exception) {
