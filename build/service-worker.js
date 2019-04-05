35a0fa15c8153a1fab8ebdccf36eb77152801e6e
pidrmasiin <petteri.paakkonen@helsinki.fi>
2019-04-06 00:35:57 +0300

    button fix

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.324c796049cd1d13c814103f84513964.js"
+  "/precache-manifest.93bf4240d9607ce0091c1e61efdea457.js"
 );
 
 workbox.clientsClaim();
