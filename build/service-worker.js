69ac38dd613878501e5fa0f6ae8a1b888cff0721
pidrmasiin <petteri.paakkonen@helsinki.fi>
2019-09-29 13:02:36 +0300

    google verification

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1caf01df35bda13ecf70ebd098b0585.js"
+  "/precache-manifest.4a57088121ca7d48a71502e6f161efa4.js"
 );
 
 self.addEventListener('message', (event) => {
