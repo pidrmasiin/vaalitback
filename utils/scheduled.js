3b62f6101d9c98558ccad85927803823d0888fb0
pidrmasiin <petteri.paakkonen@helsinki.fi>
2020-04-10 08:13:27 +0300

    schedule change

--- utils/scheduled.js
+++ utils/scheduled.js
@@ -2,7 +2,7 @@ var schedule = require('node-schedule');
 const Twit = require('twit');
 const vaskiData = require('./vaskiData')
  
-var twitterBot = schedule.scheduleJob('10 5,13,17 * * *', function(){
+var twitterBot = schedule.scheduleJob('10 4,12,16 * * *', function(){
     console.log(',', new Date());
     
         const twitSecrets = {
