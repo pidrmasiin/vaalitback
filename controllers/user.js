45e4ecc78763ba7b4a2ea7f5bddaf196cfdb7a66
pidrmasiin <petteri.paakkonen@helsinki.fi>
2023-03-06 21:12:31 +0200

    bcrypt update

--- controllers/user.js
+++ controllers/user.js
@@ -1,39 +1,42 @@
-const bcrypt = require('bcrypt')
-const userRouter = require('express').Router()
-const User = require('../models/user')
+const bcrypt = require("bcryptjs");
+const userRouter = require("express").Router();
+const User = require("../models/user");
 
+userRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users.map(User.format));
+});
 
-userRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users.map(User.format))
-})
-
-userRouter.post('/', async (request, response) => {
+userRouter.post("/", async (request, response) => {
   try {
-    const body = request.body
+    const body = request.body;
 
-    const existingUser = await User.find({ username: body.username })
+    const existingUser = await User.find({ username: body.username });
     if (existingUser.length > 0) {
-      return response.status(400).json({ error: 'username must be unique' })
-    }if (body.password.length < 3) {
-      return response.status(400).json({ error: 'password must contain at least 3 characters' })
-    }if(!body.adult){
-      body.adult = true
+      return response.status(400).json({ error: "username must be unique" });
     }
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    if (body.password.length < 3) {
+      return response
+        .status(400)
+        .json({ error: "password must contain at least 3 characters" });
+    }
+    if (!body.adult) {
+      body.adult = true;
+    }
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
     const user = new User({
       username: body.username,
       passwordHash,
-    })
+    });
 
-    const savedUser = await user.save()
-    response.json(User.format(savedUser))
+    const savedUser = await user.save();
+    response.json(User.format(savedUser));
   } catch (exception) {
-    console.log(exception)
-    response.status(500).json({ error: 'something went wrong...' })
+    console.log(exception);
+    response.status(500).json({ error: "something went wrong..." });
   }
-})
+});
 
-module.exports = userRouter
+module.exports = userRouter;
