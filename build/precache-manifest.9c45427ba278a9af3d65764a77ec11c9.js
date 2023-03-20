1042aaa575ec2f48d304247d32324e79ca7cafc7
pidrmasiin <petteri.paakkonen@helsinki.fi>
2023-03-21 00:37:54 +0200

    one more fix

---
+++ build/precache-manifest.9c45427ba278a9af3d65764a77ec11c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7687fa9c5cae1fc8f57fd2470e5f0f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10e199e5642b1869d69a",
+    "url": "/static/css/main.ca22994d.chunk.css"
+  },
+  {
+    "revision": "8ff632b56320e5aa70a1",
+    "url": "/static/js/2.2250bd3d.chunk.js"
+  },
+  {
+    "revision": "da55780e51c105d2c1812602a4bdd27a",
+    "url": "/static/js/2.2250bd3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10e199e5642b1869d69a",
+    "url": "/static/js/main.32d72d65.chunk.js"
+  },
+  {
+    "revision": "49f2c8b810e368064ed1",
+    "url": "/static/js/runtime-main.0680e990.js"
+  },
+  {
+    "revision": "17cc780bf7f08bdd9025879c99dce91f",
+    "url": "/static/media/avoin_2019.17cc780b.csv"
+  },
+  {
+    "revision": "a2212f96581f1c17b000c9616bca1115",
+    "url": "/static/media/rahoitus.a2212f96.csv"
+  },
+  {
+    "revision": "fe80cae81165b2209c36452648dc7498",
+    "url": "/static/media/vanhatvastaukset.fe80cae8.csv"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bde8ef14b23a3453a9666249cdbc394.js"
+  "/precache-manifest.9c45427ba278a9af3d65764a77ec11c9.js"
 );
 
 self.addEventListener('message', (event) => {
